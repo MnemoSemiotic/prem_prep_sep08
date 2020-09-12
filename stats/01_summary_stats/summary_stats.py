5203ff7093bb37c4993f6b2cbbae3b319e01f2c5
Tovio Roberts <clownfragment@gmail.com>
2020-09-12 12:39:45 -0600

    update

--- stats/01_summary_stats/summary_stats.py
+++ stats/01_summary_stats/summary_stats.py
@@ -5,11 +5,22 @@ Trimmed Mean
 
 a = [1, 5, 7, 10, 15, 23, 35, 67, 220, 2000]
 
-print(sum(a) / len(a)) # 238.3
+# print(sum(a) / len(a)) # 238.3
 
 a_trimmed = a[1:-1]
 # print(a_trimmed)
-print(sum(a_trimmed) / len(a_trimmed)) # 47.75
+# print(sum(a_trimmed) / len(a_trimmed)) # 47.75
 
 
+'''
+Mean
+'''
+def mean(lst, trim_by=0):
+    if trim_by > 0:
+        lst_sorted = sorted(lst)[trim_by:-trim_by]
+        sum(lst_sorted[trim_by:-trim_by]) / len(lst_sorted[trim_by:-trim_by])
+    return sum(lst) / len(lst)
 
+b = [1,2,3,4,5,6,7,8,9]
+print(mean(b))
+print(mean(b, trim_by=1))
