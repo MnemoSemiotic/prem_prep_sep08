22a54c12e2f7d89281bfa8f7a2b2cf985df746aa
Tovio Roberts <clownfragment@gmail.com>
2020-09-12 12:33:11 -0600

    update

--- stats/01_summary_stats/summary_stats.py
+++ stats/01_summary_stats/summary_stats.py
@@ -7,6 +7,9 @@ a = [1, 5, 7, 10, 15, 23, 35, 67, 220, 2000]
 
 print(sum(a) / len(a)) # 238.3
 
-a_trimmed = a[1:-2]
+a_trimmed = a[1:-1]
+# print(a_trimmed)
+print(sum(a_trimmed) / len(a_trimmed)) # 47.75
 
-print(sum(a_trimmed) / len(a_trimmed)) # 23.142
+
+
