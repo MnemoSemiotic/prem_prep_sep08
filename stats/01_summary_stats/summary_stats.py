df3125c324cd3441cccc5f010ad956583fd9fd07
Tovio Roberts <clownfragment@gmail.com>
2020-09-12 12:55:15 -0600

    update

--- stats/01_summary_stats/summary_stats.py
+++ stats/01_summary_stats/summary_stats.py
@@ -18,9 +18,22 @@ Mean
 def mean(lst, trim_by=0):
     if trim_by > 0:
         lst_sorted = sorted(lst)[trim_by:-trim_by]
-        sum(lst_sorted[trim_by:-trim_by]) / len(lst_sorted[trim_by:-trim_by])
+        return sum(lst_sorted) / len(lst_sorted)
     return sum(lst) / len(lst)
 
 b = [1,2,3,4,5,6,7,8,9]
-print(mean(b))
-print(mean(b, trim_by=1))
+# print(mean(b))
+# print(mean(b, trim_by=1))
+
+'''
+Mean Breakout
+'''
+
+urban = 6.0, 5.0, 11.0, 33.0, 4.0, 5.0, 80.0, 18.0, 35.0, 17.0, 23.0
+farmhouse = 4.0, 14.0, 11.0, 9.0, 9.0, 8.0, 4.0, 20.0, 5.0, 8.9, 21.0, 9.2, 3.0, 2.0, 0.3
+
+# print(f'Urban endotoxin mean: {mean(urban)}')
+# print(f'Farmhouse endotoxin mean: {mean(farmhouse)}')
+
+# print(f'Urban endotoxin mean trimmed(1): {mean(urban, trim_by=1)}')
+# print(f'Farmhouse endotoxin mean trimmed(1): {mean(farmhouse, trim_by=1)}')
