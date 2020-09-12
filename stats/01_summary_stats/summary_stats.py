64acb9f00af1455505d755a0e2dabb2a4672102f
Tovio Roberts <clownfragment@gmail.com>
2020-09-12 12:28:07 -0600

    update

--- stats/01_summary_stats/summary_stats.py
+++ stats/01_summary_stats/summary_stats.py
@@ -1,1 +1,12 @@
-print('hello world')
+
+'''
+Trimmed Mean
+'''
+
+a = [1, 5, 7, 10, 15, 23, 35, 67, 220, 2000]
+
+print(sum(a) / len(a)) # 238.3
+
+a_trimmed = a[1:-2]
+
+print(sum(a_trimmed) / len(a_trimmed)) # 23.142
