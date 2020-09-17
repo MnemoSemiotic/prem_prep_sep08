4235884c9af2295e25c6bbd1dfc72fb354ab1bea
Tovio Roberts <clownfragment@gmail.com>
2020-09-16 19:24:20 -0600

    Auto Updated: Wed 24:19 Sep 16 2020

--- stats/01_summary_stats/summary_stats.py
+++ stats/01_summary_stats/summary_stats.py
@@ -10,6 +10,18 @@ def mean(lst, trim_by=0):
         print(lst_)
     return sum(lst_) / len(lst_)
 
-a = [1, 5, 7, 10, 15, 23, 35, 67, 220, 2000]
+# a = [1, 5, 7, 10, 15, 23, 35, 67, 220, 2000]
 
-print(mean(a, trim_by=1))
+# print(mean(a, trim_by=1))
+
+
+
+'''
+Median
+'''
+lst1_odd = [13, 18, 13, 14, 13, 16, 14, 21, 13]
+lst2_even = [15, 14, 10, 8, 12, 8, 16, 13]
+
+print(sorted(lst1_odd))
+print(sorted(lst2_even))
+
