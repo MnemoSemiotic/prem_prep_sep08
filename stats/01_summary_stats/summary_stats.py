77e573e68ec358e758cd345cb7a2a8c7227120f3
Tovio Roberts <clownfragment@gmail.com>
2020-09-16 20:13:27 -0600

    Auto Updated: Wed 13:20 Sep 16 2020

--- stats/01_summary_stats/summary_stats.py
+++ stats/01_summary_stats/summary_stats.py
@@ -84,3 +84,27 @@ mode_lst = ['writing', 'writing', 'hiking', 'painting', 'skating', 'skating', 'w
 # print(mode(mode_lst))
 
 
+
+'''
+Five Number Summary
+Interquartile Range
+Detect Outliers
+'''
+
+def five_num_summary(lst):
+    min_ = min(lst)
+    max_ = max(lst)
+
+    med = median(lst)
+
+    sorted_lst = sorted(lst)
+
+    print(sorted_lst)
+
+    return min_, max_, med
+
+a = [15,2,9,5,6,7,27,12,18,19,1]
+b = [6,1,4,51,7,16,10,14,46,22,24,56,48,54]
+
+print(five_num_summary(a))
+print(five_num_summary(b))
