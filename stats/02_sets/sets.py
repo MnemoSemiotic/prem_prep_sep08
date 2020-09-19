5557a6951e97fef2859dde8e9b4c8f35afd1aa8e
Tovio Roberts <clownfragment@gmail.com>
2020-09-19 14:05:09 -0600

    Auto Updated: Sat 05:14 Sep 19 2020

--- stats/02_sets/sets.py
+++ stats/02_sets/sets.py
@@ -177,4 +177,12 @@ list3 = ['bear', 'whale', 'sea cucumber', 'mink', 'eagle', 'dog']
 
 
 def intersection(set1, set2):
-    pass
+    set_intersect = []
+
+    for item in set1:
+        if item in set2:
+            set_intersect.append(item)
+
+    return set_intersect
+
+print(intersection(list1, list2))
