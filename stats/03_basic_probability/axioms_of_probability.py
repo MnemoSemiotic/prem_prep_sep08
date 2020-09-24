a768a42abdfc200e6dfe26768e36d1849524efd6
Tovio Roberts <clownfragment@gmail.com>
2020-09-23 19:20:20 -0600

    Auto Updated: Wed 20:19 Sep 23 2020

--- stats/03_basic_probability/axioms_of_probability.py
+++ stats/03_basic_probability/axioms_of_probability.py
@@ -4,12 +4,15 @@ setA = set(['bear', 'cat', 'dog', 'dolphin', 'weasel'])
 setB = set(['bear', 'dog', 'elephant', 'weasel', 'mink', 'mountain lion'])
 setC = set(['bear', 'whale', 'sea cucumber', 'mink', 'eagle', 'dog'])
 
+sample_space = setA.union(setB).union(setC)
+
 
 '''
 Commutative
 A ∪ B = B ∪ A
 AB = BA
 '''
+print(setA.union(setB) == setB.union(setA))
 
 
 
