3193da6ff6a921da9df80a1597b748380174aa6e
Tovio Roberts <clownfragment@gmail.com>
2020-09-23 19:13:09 -0600

    Auto Updated: Wed 13:19 Sep 23 2020

--- stats/03_basic_probability/axioms_of_probability.py
+++ stats/03_basic_probability/axioms_of_probability.py
@@ -1,13 +1,31 @@
 
+
+setA = set(['bear', 'cat', 'dog', 'dolphin', 'weasel'])
+setB = set(['bear', 'dog', 'elephant', 'weasel', 'mink', 'mountain lion'])
+setC = set(['bear', 'whale', 'sea cucumber', 'mink', 'eagle', 'dog'])
+
+
 '''
 Commutative
 A ∪ B = B ∪ A
 AB = BA
 '''
 
+
+
+
+
+
+
+
+'''
 Associative
 (A ∪ B) ∪ C = A ∪ (B ∪ C) = A ∪ B ∪ C ⇒ 5 + (6 + 7) = (5 + 6 ) + 7 = 5 + 6 + 7
+'''
+
+'''
 (AB)C = A(BC) = ABC
 Distributive
 A ∪ (BC) = (A ∪ B)(A ∪ C) 
 A(B ∪ C) = (AB) ∪ (AC)
+'''
