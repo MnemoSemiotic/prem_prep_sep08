fb5a5afc42cce90f074bb88663673c9989287b77
Tovio Roberts <clownfragment@gmail.com>
2020-09-23 19:12:05 -0600

    Auto Updated: Wed 12:19 Sep 23 2020

---
+++ stats/03_basic_probability/axioms_of_probability.py
@@ -0,0 +1,13 @@
+
+'''
+Commutative
+A ∪ B = B ∪ A
+AB = BA
+'''
+
+Associative
+(A ∪ B) ∪ C = A ∪ (B ∪ C) = A ∪ B ∪ C ⇒ 5 + (6 + 7) = (5 + 6 ) + 7 = 5 + 6 + 7
+(AB)C = A(BC) = ABC
+Distributive
+A ∪ (BC) = (A ∪ B)(A ∪ C) 
+A(B ∪ C) = (AB) ∪ (AC)
