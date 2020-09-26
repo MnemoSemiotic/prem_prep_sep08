6b41c149cb629a7867dc5bf6fd3e8c4e3fe5843f
Tovio Roberts <clownfragment@gmail.com>
2020-09-26 14:15:32 -0600

    Auto Updated: Sat 15:14 Sep 26 2020

---
+++ stats/05_discrete_distributions/bernoulli.py
@@ -0,0 +1,14 @@
+'''
+Bernoulli trial (aka distribution)
+
+single event with a binary outcome, with a set probability
+
+If you have a coin that on average turns up heads in 90% of its flips, what is the probability that you'll get heads?
+0.9
+what is the probability that you'll get tails?
+0.1
+'''
+
+from random import random
+def bernoulli(p_success=0.5):
+    pass
