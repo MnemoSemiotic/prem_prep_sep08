25c8a75b9eee87ee692cd6bfb0e8fd649892cb87
Tovio Roberts <clownfragment@gmail.com>
2020-09-28 19:54:55 -0600

    Auto Updated: Mon 54:19 Sep 28 2020

--- stats/05_discrete_distributions/binomial/binomial_counting.py
+++ stats/05_discrete_distributions/binomial/binomial_counting.py
@@ -13,3 +13,37 @@ def gen_4_bit_binary():
 # for dec, bin_ in gen_4_bit_binary().items():
 #     print(f'{dec}: {bin_}')
 
+
+'''
+7 minutes!
+
+Code the dec_to_bin function. 
+Should take as input 2 things:
+    a decimal value (dec)
+    a number of bits (n_bits=8)
+
+algorithm
+Given a decimal number
+    - take the modulus by 2
+        - set aside the result
+    - floor divide the number by 2
+        - until that number is 0
+    - reverse the sequence of outcomes
+
+dec_to_bin(43)
+--------------
+43 % 2     ==> 1
+43 // 2 = 21
+21 % 2     ==> 1
+21 // 2 = 10
+10 % 2     ==> 0
+10 // 2 = 5
+5 % 2      ==> 1
+5 // 2  = 2
+2 % 2      ==> 0
+2 // 2  = 1
+1 % 2      ==> 1
+1 // 2  = 0
+
+reversed ==> 101011 is the binary representation of 43
+'''
