12c4d5e5903e50e35b07241348ce992b8168d6ee
Tovio Roberts <clownfragment@gmail.com>
2020-09-29 19:27:57 -0600

    Auto Updated: Tue 27:19 Sep 29 2020

--- stats/05_discrete_distributions/binomial/binomial_using_pmf.py
+++ stats/05_discrete_distributions/binomial/binomial_using_pmf.py
@@ -73,6 +73,13 @@ p = 3/7
 # print(binomial_pmf(10, 5, p=(3/7)))
 
 
+'''
+In 30 vehicles, it is known that 2 of them will be motorcycles. Knowing this, in 40 vehicles, what is the probability 5 of them will be motorcycles?
+'''
+p = 2/30
+n = 40
+k = 5
+# print(binomial_pmf(n, k, p))
 
 
 '''
