a015e0fa85b2b5350144937a3e00f68e3715ae42
Tovio Roberts <clownfragment@gmail.com>
2020-09-29 20:09:21 -0600

    Auto Updated: Tue 09:20 Sep 29 2020

--- stats/05_discrete_distributions/binomial/binomial_sampling.py
+++ stats/05_discrete_distributions/binomial/binomial_sampling.py
@@ -13,5 +13,9 @@ def generate_n_bits(n=8):
 # print(generate_n_bits(12))
 
 '''
-Write a function called binary_sampling_dict
+Write a function called binary_sampling_dict that has two parameters
+    num_bits
+    num_samples
+
+return a dictionary where the keys represent the number of successes, and the values associated with those keys represent the count of that number of successes occurring
 '''
