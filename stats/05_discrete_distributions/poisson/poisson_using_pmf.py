6659085ef556519b8196778dd5133172b15c9b65
Tovio Roberts <clownfragment@gmail.com>
2020-10-01 21:05:05 -0600

    Auto Updated: Thu 05:21 Oct 01 2020

--- stats/05_discrete_distributions/poisson/poisson_using_pmf.py
+++ stats/05_discrete_distributions/poisson/poisson_using_pmf.py
@@ -249,3 +249,15 @@ def a_proba_dict():
 
 # for k, v in a_proba_dict().items():
 #     print(f'{k}: {v:}')
+
+
+
+
+def poisson_counts(lmbda, low_k, high_k, num_samples=10000):
+  d = dict()
+  for k in range(low_k, high_k):
+    d[k] = round(poisson_pmf(lmbda, k) * num_samples)
+  return d 
+d = poisson_counts(10, 0, 30, num_samples=10000)
+for k, v in d.items():
+  print(f'{k}:{v/sum(d.values())}')
