8615bd8bdbcd2deaf874dc6381ee664f1cf63856
Tovio Roberts <clownfragment@gmail.com>
2020-10-01 20:10:19 -0600

    Auto Updated: Thu 10:20 Oct 01 2020

--- stats/05_discrete_distributions/geometric/geometric_using_pmf.py
+++ stats/05_discrete_distributions/geometric/geometric_using_pmf.py
@@ -73,3 +73,9 @@ you get your first heads before or on the 7th flip?
 # print(geom_cdf_closed(0.5, 6, inclusive=False))
 # print(geom_cdf_accum(0.5, 7, inclusive=True))
 # print(geom_cdf_accum(0.5, 6, inclusive=False))
+
+'''
+geometric pmf dictionary
+'''
+def geometric_pmf_dict(p, k_high, inclusive=True):
+    pass
