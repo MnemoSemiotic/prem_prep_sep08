3760d54eabc4a0d3a45d2e835055dbdccaa16f91
Tovio Roberts <clownfragment@gmail.com>
2020-10-01 20:47:32 -0600

    Auto Updated: Thu 47:20 Oct 01 2020

--- stats/05_discrete_distributions/geometric/geometric_problems.py
+++ stats/05_discrete_distributions/geometric/geometric_problems.py
@@ -6,4 +6,32 @@ Suppose you have an unfair coin, with a 68% chance of getting tails.
 What is the probability that the first head will be on the 3rd trial?
 '''
 
-print(geometric_pmf(p=0.32, k=3, inclusive=True))
+# print(geometric_pmf(p=0.32, k=3, inclusive=True))
+
+'''
+Geometric Breakout 2
+According to the article “Characterizing the Severity and Risk of Drought in the Poudre River, Colorado” taken from a water conservation journal, the drought length Y is the number of consecutive time intervals in which the water supply remains below a critical value y0 (a deficit), preceded by and followed by periods in which the supply exceeds this critical value (a surplus). The journal proposes a geometric distribution with  p = 0.409 for this random variable.
+(hint: consider the probability p to represent the “success” of breaking a drought)
+What is the probability that the drought lasts exactly three intervals?
+'''
+
+'''
+Given that there is a drought, what is the probability that the drought lasts at most three intervals?
+'''
+
+
+
+
+'''
+Differentiating Binomial, Poisson, Geometric, and basic probability
+'''
+# Binomial
+# What is the probability that some number out of some number of events is successful?
+# p=0.7, n_trial = 5, what is the proba that 3 of the 5 trials are successful?
+
+# Poisson
+# On average, some number of events happens in this volume (of time, space, etc). 
+# What is the probability of some (other) number of events happens in this volume or 
+# some expansion/contraction of this volume?
+# On avg 14 events occur in 5 minutes, what is the probability that 20 events occur 
+# in 8 minutes?
