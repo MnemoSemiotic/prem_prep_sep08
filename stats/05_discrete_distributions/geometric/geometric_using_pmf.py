84a3f65c99cc7c1a106598833f3aaec78bdbea02
Tovio Roberts <clownfragment@gmail.com>
2020-10-01 19:27:08 -0600

    Auto Updated: Thu 27:19 Oct 01 2020

---
+++ stats/05_discrete_distributions/geometric/geometric_using_pmf.py
@@ -0,0 +1,15 @@
+'''
+Geometric Distribution
+- models the number of failures up to the first success
+    - two formulas
+        - up to but not including the first success
+        - up to and including the first success
+
+k will represent the # of failures (inclusive or exclusive of the first success)
+
+0000001
+001
+0000000000001
+
+You are handing out fliars for a show. The probability of someone accepting a fliar is 0.65. What is the probability that the first person who accepts a fliar is the 3rd person you offer a fliar?
+'''
