20ad88eb031f410b1356e333ba727a6de3f3fba4
Tovio Roberts <clownfragment@gmail.com>
2020-10-07 20:30:03 -0600

    Auto Updated: Wed 30:20 Oct 07 2020

--- stats/06_continuous_distributions/normal.py
+++ stats/06_continuous_distributions/normal.py
@@ -10,6 +10,15 @@ def normal_cdf(x=0, mu=0, sigma=2):
     for num in range(-10000, int(x*1000)):
         vals.append(num*0.001)
 
-    print(vals)
+    area_accum = 0.0
+
+    for val in vals:
+        res = normal_pdf(val, mu, sigma)
+        accum += res
+
+        if val > x:
+            break
+
+    return area_accum*0.001
 
 print(normal_cdf())
