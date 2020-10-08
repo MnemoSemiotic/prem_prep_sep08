41d6263391863a9f023b411ab1aa581d14009292
Tovio Roberts <clownfragment@gmail.com>
2020-10-07 20:52:51 -0600

    Auto Updated: Wed 52:20 Oct 07 2020

--- stats/06_continuous_distributions/exponential.py
+++ stats/06_continuous_distributions/exponential.py
@@ -27,11 +27,11 @@ X = The amount of time (in minutes) that you have to wait.
 X ~ exponential(0.1)
 '''
 
-# What’s the probability that you’ll have to wait more than ten minutes? 
-print(1-exponential_cdf(0.1, 10))
+# # What’s the probability that you’ll have to wait more than ten minutes? 
+# print(1-exponential_cdf(0.1, 10))
 
-# What is the amount of time that you would expect to wait? 
-print(exponential_mean(0.1))
+# # What is the amount of time that you would expect to wait? 
+# print(exponential_mean(0.1))
 
-# What is the variance of this random variable?
-print(exponential_variance(0.1))
+# # What is the variance of this random variable?
+# print(exponential_std(0.1))
