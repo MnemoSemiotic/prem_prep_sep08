40fa11f6f1187d9aac3f9bdf8a5d1eebbb5a8050
Tovio Roberts <clownfragment@gmail.com>
2020-10-07 19:42:15 -0600

    Auto Updated: Wed 42:19 Oct 07 2020

--- stats/06_continuous_distributions/uniform.py
+++ stats/06_continuous_distributions/uniform.py
@@ -42,3 +42,13 @@ def get_binary(n=8):
 
 # print(get_binary(32))
 
+def get_float(n=8):
+    bin_list = get_binary(n)
+
+    float_accum = 0.0
+
+    for idx, bit in enumeration(bin_list, 1):
+        float_accum += bit * 0.5**idx
+
+    return float_accum
+
