38ef52a2db0de519096de304a1ea45cf25ba212a
Tovio Roberts <clownfragment@gmail.com>
2020-10-07 20:18:37 -0600

    Auto Updated: Wed 18:20 Oct 07 2020

--- stats/06_continuous_distributions/normal.py
+++ stats/06_continuous_distributions/normal.py
@@ -2,3 +2,5 @@ from math import e, pi, sqrt
 
 def normal_pdf(x, mu, sigma):
     return 1 / sqrt(2 * pi * sigma**2) * e**((- (x - mu)**2) / 2 * sigma**2)
+
+print(normal_pdf(x=1.96, mu=0, sigma=1))
