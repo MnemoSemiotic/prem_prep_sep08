dbe849431d7f40ae025e29086cc02728c85b2d69
Tovio Roberts <clownfragment@gmail.com>
2020-10-07 19:39:09 -0600

    Auto Updated: Wed 39:19 Oct 07 2020

--- stats/06_continuous_distributions/uniform.py
+++ stats/06_continuous_distributions/uniform.py
@@ -30,3 +30,15 @@ from random import choice
 
 def get_bit():
     return choice([0,1])
+
+def get_binary(n=8):
+    # return [get_bit for _ in range(n)]
+    return_list = []
+
+    for _ in range(n):
+        return_list.append(get_bit())
+    
+    return return_list
+
+# print(get_binary(32))
+
