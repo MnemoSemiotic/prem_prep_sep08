8bcb5e211e20c69640e32c478434d019c8b275b0
Tovio Roberts <clownfragment@gmail.com>
2020-10-07 20:51:48 -0600

    Auto Updated: Wed 51:20 Oct 07 2020

--- stats/06_continuous_distributions/exponential.py
+++ stats/06_continuous_distributions/exponential.py
@@ -16,3 +16,22 @@ def exponential_variance(lmbda):
 
 def exponential_std(lmbda):
     return sqrt(exponential_variance(lmbda))
+
+'''
+Exponential Breakout 1
+
+Suppose you’re on a street corner trying to hail a taxi cab. Let 
+
+X = The amount of time (in minutes) that you have to wait. 
+
+X ~ exponential(0.1)
+'''
+
+# What’s the probability that you’ll have to wait more than ten minutes? 
+print(1-exponential_cdf(0.1, 10))
+
+# What is the amount of time that you would expect to wait? 
+print(exponential_mean(0.1))
+
+# What is the variance of this random variable?
+print(exponential_variance(0.1))
