08985575644431beb64289b116926b02d06f8aca
Tovio Roberts <clownfragment@gmail.com>
2020-10-10 12:22:26 -0600

    Auto Updated: Sat 22:12 Oct 10 2020

--- stats/08_preparing_for_ti/ti_prep.py
+++ stats/08_preparing_for_ti/ti_prep.py
@@ -1,6 +1,31 @@
 ''' TI Style Questions (2020-10-10) '''
 
+'''
 
+TI Skills/Outline
+
+* textbook problems re: Discrete Distrs
+    * Recognize and solve problems related to:
+        * Binomial
+        * Poisson
+        * Geometric
+        * Uniform
+    
+    * Understanding counting or binary through the lens of independent trials
+        * binary ex: [0,1,1,0,1] <- each "coin flip or bit is independent of the other, if each bit is randomly generated"
+        * trinary ex: [0, 1, 1, 2, 0]
+
+    * Coding mathematical formulations
+
+    * Analysis through Dictionaries
+        * Pack values into dictionaries
+            * Check membership vs not checking membership
+        * Transform Dictionary values
+        * General analytic approach
+            * Create generative process
+            * Load results into a dict
+            * Interpret
+'''
 
 
 
