aad4c84b573813b2a80da0b0510d636dcc196df6
Tovio Roberts <clownfragment@gmail.com>
2020-10-10 11:27:25 -0600

    Auto Updated: Sat 27:11 Oct 10 2020

---
+++ stats/08_preparing_for_ti/ti_prep.py
@@ -0,0 +1,35 @@
+''' TI Style Questions (2020-10-10) '''
+
+'''
+
+Binomial textbook problems
+
+'''
+
+
+'''
+
+Poisson textbook problems
+
+'''
+
+
+'''
+
+Random Variables
+
+'''
+
+
+'''
+
+Coding Mathematical Formulations
+
+'''
+
+
+'''
+
+Analysis using Dictionaries
+
+'''
