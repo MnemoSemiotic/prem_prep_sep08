89f8bf64d538dafbc97dffb624950fb24196ba6d
Tovio Roberts <clownfragment@gmail.com>
2020-10-10 12:28:36 -0600

    Auto Updated: Sat 28:12 Oct 10 2020

--- stats/08_preparing_for_ti/ti_prep.py
+++ stats/08_preparing_for_ti/ti_prep.py
@@ -33,7 +33,34 @@ TI Skills/Outline
 Binomial textbook problems
 
 You are sitting on a dock watching boats go by. On average, two out of every 13 boats that goes by has shipping containers on it. What is the probability that, in one particular set of observations, 10 out of 20 boats have shipping containers on them?
+
+p = 2/13
+k = 10
+n = 20
+
+binomial_pmf(n=20, k=10, p=(2/13) )
 '''
+def factorial(n):
+    prod = 1
+    for num in range(1, 1+n):
+        prod *= num
+    return prod
+
+def combinations(n, k):
+    return int(factorial(n) / (factorial(n-k) * factorial(k)))
+
+def binomial_pmf(n, k, p=0.5):
+    return combinations(n, k) * (p**k) * ((1-p)**(n-k))
+
+
+print(binomial_pmf(n=20, k=10, p=(2/13))
+
+
+
+
+
+
+
 
 
 '''
